bcd18a1219b131be7f0084526086ca6e27a4f7f9
ElizabethVia <50150253+ElizabethVia@users.noreply.github.com>
2019-08-07 15:25:26 -0400

    Delete LoginTests.java

--- src/test/java/LoginTests.java
+++
@@ -1,9 +0,0 @@
-public class LoginTests {
-
-        public static void main(String[] args) {
-            System.out.println("line 1");
-            System.out.println("line 2");
-        }
-    }
-
-
