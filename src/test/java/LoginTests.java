7bc8b1274a89f26fd82d3adf6c0bde44efe0ffc7
Elizabeth <Elizabethvia32@gmail.com>
2019-08-13 19:44:06 -0400

    changed the file in new branch

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,6 +3,11 @@ public class LoginTests {
         public static void main(String[] args) {
             System.out.println("line 1");
             System.out.println("line 2");
+            System.out.println("line 2");
+            System.out.println("line 2");
+            System.out.println("line 23");
+            System.out.println("line 244");
+            System.out.println("line 211");
         }
     }
 
